7d432ee401d0af5d9645e3987b92566a45caa336
Lucas Pizol <lucaspizolfe@gmail.com>
2023-12-23 18:17:41 -0300

    Adicionando a impressão do tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -3,6 +3,7 @@ namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
             Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            Tela.ImprimirTabuleiro(tabuleiro);
 
             Console.ReadLine();
         }
