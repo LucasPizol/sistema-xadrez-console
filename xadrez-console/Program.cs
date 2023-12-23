0b8f1c79d8df7e60703534821e54877a2457e8b7
Lucas Pizol <lucaspizolfe@gmail.com>
2023-12-23 17:44:25 -0300

    Adicionando classe de posição

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,6 +1,12 @@
-﻿namespace xadrez_console {
+﻿using tabuleiro;
+namespace xadrez_console {
     class Program {
-        static void Main(string[] args) { }
+        static void Main(string[] args) {
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
+        }
     }
 
 }
